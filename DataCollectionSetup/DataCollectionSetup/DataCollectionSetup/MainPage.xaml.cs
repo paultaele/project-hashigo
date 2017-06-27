cdf3da94fd5204e3b43a4fe1adf54bba213ebf26
Paul Taele <ptaele@gmail.com>
2017-06-26 23:33:13 -0500

    Added DataCollectionSetup project

---
+++ DataCollectionSetup/DataCollectionSetup/DataCollectionSetup/MainPage.xaml.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.ViewManagement;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace DataCollectionSetup
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            ApplicationView.PreferredLaunchWindowingMode = ApplicationViewWindowingMode.FullScreen;
+        }
+
+        private void OpenFileButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void SaveButton_Click(object sender, RoutedEventArgs e)
+        {
+            ;
+        }
+
+        private void AddPromptElementsButton_Click(object sender, RoutedEventArgs e)
+        {
+            PromptElement currentPromptElement = new PromptElement(MyPromptElementsStack.Children.Count, MyCounter);
+            currentPromptElement.Name = "myPromptElement" + MyCounter;
+            MyCounter++;
+
+            MyPromptElementsStack.Children.Add(currentPromptElement);
+        }
+
+        private void RemovePromptElementsButton_Click(object sender, RoutedEventArgs e)
+        {
+            var promptElementsToDelete = new List<PromptElement>();
+            foreach (PromptElement promptElement in MyPromptElementsStack.Children)
+            {
+                if (promptElement.IsChecked)
+                {
+                    promptElementsToDelete.Add(promptElement);
+                }
+            }
+
+            if (promptElementsToDelete.Count == 0) { return; }
+
+            foreach (var promptElementToDelate in promptElementsToDelete)
+            {
+                MyPromptElementsStack.Children.Remove(promptElementToDelate);
+            }
+
+            for (int i = 0; i < MyPromptElementsStack.Children.Count; ++i)
+            {
+                var promptElement = (PromptElement)(MyPromptElementsStack.Children[i]);
+                promptElement.PositionName = "" + i;
+            }
+        }
+
+        private void InsertPromptElementsButton_Click(object sender, RoutedEventArgs e)
+        {
+            // check if the value is valid
+            string value = InsertIndexText.Text;
+            int index;
+            if (!int.TryParse(value, out index)) {
+                InsertIndexText.Text = ""; // clear the text box
+                return; // do nothing
+            }
+
+            //
+            int count = MyPromptElementsStack.Children.Count;
+
+            // case: stack is empty
+            if (count == 0)
+            {
+                PromptElement currentPromptElement = new PromptElement(0, MyCounter);
+                currentPromptElement.Name = "myPromptElement" + MyCounter;
+                MyCounter++;
+
+                MyPromptElementsStack.Children.Add(currentPromptElement);
+            }
+
+            // case: index exceeds stack size
+            else if (index >= count)
+            {
+                PromptElement currentPromptElement = new PromptElement(count, MyCounter);
+                currentPromptElement.Name = "myPromptElement" + MyCounter;
+                MyCounter++;
+
+                MyPromptElementsStack.Children.Add(currentPromptElement);
+            }
+
+            //
+            else
+            {
+                PromptElement currentPromptElement = new PromptElement(index, MyCounter);
+                currentPromptElement.Name = "myPromptElement" + MyCounter;
+                MyCounter++;
+
+                //
+                MyPromptElementsStack.Children.Insert(index, currentPromptElement);
+                for (int i = index + 1; i < MyPromptElementsStack.Children.Count; ++i)
+                {
+                    var promptElement = (PromptElement)MyPromptElementsStack.Children[i];
+                    promptElement.PositionName = "" + i;
+                }
+            }
+
+            this.InsertIndexText.Text = "";
+        }
+
+        private int MyCounter { get; set; }
+    }
+}
---
+++ DataCollectionSetup/DataCollectionSetup/DataCollectionSetup/PromptElement.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The User Control item template is documented at http://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace DataCollectionSetup
+{
+    public sealed partial class PromptElement : UserControl
+    {
+        public PromptElement(int position, int count)
+        {
+            this.InitializeComponent();
+
+            //
+            this.MyPositionText.Text = "" + position;
+
+            //
+            this.MyTraceButton.GroupName = DISPLAY_GROUP_NAME + "_" + count;
+            this.MyReferenceButton.GroupName = DISPLAY_GROUP_NAME + "_" + count;
+            this.MyMemoryButton.GroupName = DISPLAY_GROUP_NAME + "_" + count;
+        }
+
+        public bool IsChecked { get { return MyRemoveCheckBox.IsChecked.Value; } }
+        public string ImageFileName {  get { return MyImageFileNameText.Text; } }
+        public string LabelName { get { return MyLabelText.Text; } }
+        public string CountName { get { return MyCountText.Text; } }
+        public string PositionName { get { return MyPositionText.Text; } set { MyPositionText.Text = value; } }
+
+        public static readonly String DISPLAY_GROUP_NAME = "DisplayGroup";
+    }
+}
